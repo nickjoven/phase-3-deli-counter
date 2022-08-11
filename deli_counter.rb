a3fa628ec780bbf30e50467cb0ca83d78da6ed66
Nicholas <nicholasjoven@gmail.com>
2022-08-10 21:01:14 -0400

    commit tree 619d11426da78382e3049b34a6f730c28de7ef58

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def line(queue)
+  if queue.empty? == true
+    puts "The line is currently empty." 
+  else
+    say = "The line is currently:" 
+    queue.each.with_index(1) { |customer, position| say << " #{position}. #{customer}"}
+    puts say
+  end
+end
+
+def take_a_number(queue, new_customer)
+    queue << new_customer
+    puts "Welcome, #{new_customer}. You are number #{queue.length} in line."
+end
+
+def now_serving(queue)
+    if queue.empty? == true
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{queue.shift()}."
+    end
+end
+
+
